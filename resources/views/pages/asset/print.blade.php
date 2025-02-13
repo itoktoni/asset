41943ea6831a8a0a1faa1b5db00810e529a70cb0
itoktoni <itok.toni@gmail.com>
2025-02-13 21:52:59 +0700

    update

--- resources/views/pages/asset/print.blade.php
+++ resources/views/pages/asset/print.blade.php
@@ -36,15 +36,15 @@
 <body>
     <div class="container" style="text-align: center;">
         <p style="font-size: 5px;margin-top:0px;margin-bottom:0px">.</p>
-        <h5 style="margin-top:5px;font-size:12px;margin-bottom:2rem;">
+        <h5 style="margin-top:5px;font-size:12px;margin-bottom:20px;">
             <span style="position: absolute; left:0px;font-size:5px">.</span>
-            <span style="padding-left: 5px;padding-right:10px;font-size:2.5rem">{{ $model->field_name }}</span>
+            <span style="padding-left: 5px;padding-right:10px;font-size:1rem">{{ $model->field_name }}</span>
             <span style="position: absolute; right:0px;font-size:5px">.</span>
         </h5>
         <h5 style="margin: 0px auto;text-align:center;">
-			{!! DNS2D::getBarcodeSVG(route('detail_asset', ['code' => $model->field_primary]), 'QRCODE', 15, 15) !!}
+			{!! DNS2D::getBarcodeSVG(route('detail_asset', ['code' => $model->field_primary]), 'QRCODE', 5, 5) !!}
         </h5>
-        <h5 style="margin-top:2rem;font-size:3rem;margin-bottom:0px">{{ $model->field_serial_number }}</h5>
+        <h5 style="margin-top:0rem;font-size:1.5rem;margin-bottom:0px">{{ $model->field_serial_number }}</h5>
         <p style="margin-top:0px;margin-bottom:0px;font-size:5px">.</p>
     </div>
 </body>
