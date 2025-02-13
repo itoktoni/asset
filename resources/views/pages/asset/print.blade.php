fbbd4edef3c69044da1043910ce8271c917bf6d8
itoktoni <itok.toni@gmail.com>
2025-02-13 21:37:30 +0700

    update

--- resources/views/pages/asset/print.blade.php
+++ resources/views/pages/asset/print.blade.php
@@ -2,57 +2,65 @@
 <html lang="en">
 
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>Print Barcode Product</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Print Barcode Product</title>
 
-	<style>
-	.page-break {
-		page-break-after: always;
-	}
+    <style>
+        .page-break {
+            page-break-after: always;
+        }
 
-	@page {
-		margin: 0;
-		padding: 0;
-	}
+        @page {
+            margin: 0;
+            padding: 0;
+        }
 
-	body {
-		font-family: Times New Roman;
-		font-size: 33px;
-		text-align: center;
-	}
+        @media print {
+            @page {
+                size: 580mm 400mm;
+                margin: 10mm;
+            }
+        }
 
-	.container {
-		text-align: left;
-		width: 100%;
-		margin-left: 5px;
-	}
-	</style>
+        body {
+            font-family: Times New Roman;
+            font-size: 33px;
+            text-align: center;
+        }
+
+        .container {
+            text-align: left;
+            width: 100%;
+            margin-left: 5px;
+        }
+    </style>
 
 </head>
 
 <body>
-	<div class="container" style="text-align: center;">
-		<p style="font-size: 5px;margin-top:0px;margin-bottom:0px">.</p>
-		<h5 style="margin-top:5px;font-size:12px;margin-bottom:0px;">
-			<span style="position: absolute; left:0px;font-size:5px">.</span>
-			<span style="padding-left: 5px;padding-right:10px">{{ $model->field_name }}</span>
-			<span style="position: absolute; right:0px;font-size:5px">.</span>
-		</h5>
-		<h5 style="margin: 0px auto;text-align:center">
-			<img style="margin-top:10px;height:70px" src="data:image/png;base64,{{BARCODE2D::getBarcodePNG(route('detail_asset', ['code' => $model->field_primary]), 'QRCODE')}}"
-			alt="barcode" />
-		</h5>
-		<h5 style="margin-top:3px;font-size:15px;margin-bottom:0px">{{ $model->field_serial_number }}</h5>
-		<p style="margin-top:0px;margin-bottom:0px;font-size:5px">.</p>
-	</div>
+    <div class="container" style="text-align: center;">
+        <p style="font-size: 5px;margin-top:0px;margin-bottom:0px">.</p>
+        <h5 style="margin-top:5px;font-size:12px;margin-bottom:0px;">
+            <span style="position: absolute; left:0px;font-size:5px">.</span>
+            <span style="padding-left: 5px;padding-right:10px">{{ $model->field_name }}</span>
+            <span style="position: absolute; right:0px;font-size:5px">.</span>
+        </h5>
+        <h5 style="margin: 0px auto;text-align:center">
+            <img style="margin-top:10px;height:70px"
+                src="data:image/png;base64,{{ BARCODE2D::getBarcodePNG(route('detail_asset', ['code' => $model->field_primary]), 'QRCODE') }}"
+                alt="barcode" />
+        </h5>
+        <h5 style="margin-top:3px;font-size:15px;margin-bottom:0px">{{ $model->field_serial_number }}</h5>
+        <p style="margin-top:0px;margin-bottom:0px;font-size:5px">.</p>
+    </div>
 </body>
 
 <script>
-	window.print();
-	Website2APK.printPage();
+    window.print();
+    Website2APK.printPage();
 </script>
 
 </html>
