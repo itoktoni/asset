c680665161dc401ee95af2f5198b0fba6580c8da
itoktoni <itok.toni@gmail.com>
2025-02-21 00:42:30 +0700

    update

--- resources/views/core/home/dashboard.blade.php
+++ resources/views/core/home/dashboard.blade.php
@@ -116,8 +116,8 @@
                 <tbody>
                     <tr>
                         <td class="text-center">{{ $total_asset }}</td>
-                        <td class="text-center">{{ $total_kalibrasi_belum_expired }}</td>
                         <td class="text-center">{{ $total_kalibrasi_expired }}</td>
+                        <td class="text-center">{{ $total_kalibrasi_belum_expired }}</td>
                     </tr>
                 </tbody>
             </table>
