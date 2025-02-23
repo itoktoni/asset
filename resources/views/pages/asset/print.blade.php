fb9b552b2b69dbb56d0cf02f340675ec3d37a8fa
itoktoni <itok.toni@gmail.com>
2025-02-23 16:37:35 +0700

    update

--- resources/views/pages/asset/print.blade.php
+++ resources/views/pages/asset/print.blade.php
@@ -42,7 +42,7 @@
         <p style="font-size: 5px;margin-top:0px;margin-bottom:0px">.</p>
         <h5 style="margin-top:1rem;font-size:12px;margin-bottom:20px;">
             <span style="position: absolute; left:0px;font-size:5px">.</span>
-            <span style="padding-left: 5px;padding-right:10px;font-size:1rem">
+            <span style="padding-left: 5px;padding-right:10px;font-size:2rem">
 				@php
 				$nama = $model->field_name;
 				$tambahan = '';
@@ -76,9 +76,9 @@
             <span style="position: absolute; right:0px;font-size:5px">.</span>
         </h5>
         <h5 class="barcode" style="margin: 0px auto;text-align:center;">
-			{!! DNS2D::getBarcodeHTML(route('detail_asset', ['id' => $model->field_primary]), 'QRCODE', 3, 3) !!}
+			{!! DNS2D::getBarcodeHTML(route('detail_asset', ['id' => $model->field_primary]), 'QRCODE', 8, 8) !!}
         </h5>
-        <h5 style="margin-top:1rem;font-size:2rem;margin-bottom:0px">{{ $model->field_serial_number }}</h5>
+        <h5 style="margin-top:1rem;font-size:2.5rem;margin-bottom:0px">{{ $model->field_serial_number }}</h5>
         <p style="margin-top:0px;margin-bottom:0px;font-size:5px">.</p>
     </div>
 </body>
