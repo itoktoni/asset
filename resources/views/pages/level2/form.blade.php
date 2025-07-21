5598e04a69c8e320e845ba6b96b40eaec27e7c22
itok toni laksono <itok.toni@gmail.com>
2025-07-21 23:40:49 +0700

    update

--- resources/views/pages/level2/form.blade.php
+++ resources/views/pages/level2/form.blade.php
@@ -5,7 +5,7 @@
 
                 @bind($model)
 
-                <x-form-select col="6" name="level2_id_level1" label="{{ env('LEVEL_1', 'Level 1') }}" :options="$level1" />
+                <x-form-select col="6" name="level2_code_level1" label="{{ env('LEVEL_1', 'Level 1') }}" :options="$level1" />
                 <x-form-input col="6" name="level2_nama" />
                 <x-form-textarea col="12" name="level2_keterangan" />
 
