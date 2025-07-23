453fb7a912725e8f358b0174ddfb9de4154ac04b
itok toni laksono <itok.toni@gmail.com>
2025-07-23 10:09:58 +0700

    update create inventaris

--- app/Dao/Models/Asset.php
+++ app/Dao/Models/Asset.php
@@ -3,6 +3,8 @@
 namespace App\Dao\Models;
 
 use App\Dao\Entities\Core\AssetEntity;
+use App\Dao\Enums\JobStatusType;
+use App\Dao\Enums\JobType;
 use App\Dao\Models\Core\SystemModel;
 use App\Facades\Model\AssetModel;
 use App\Facades\Model\DepartmentModel;
@@ -14,6 +16,7 @@ use App\Facades\Model\ModelModel;
 use App\Facades\Model\PenamaanModel;
 use App\Facades\Model\VendorModel;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\DB;
 use Wildside\Userstamps\Userstamps;
 use Plugins\Query;
 
@@ -198,6 +201,18 @@ class Asset extends SystemModel
 
     public static function boot()
     {
+        parent::created(function ($model)
+        {
+            Job::Create([
+                Job::field_assign_id() => auth()->user()->id,
+                Job::field_type() => JobType::Inventaris,
+                Job::field_status() => JobStatusType::Selesai,
+                Job::field_asset_id() => DB::getPdo()->lastInsertId(),
+                Job::field_location_id() => $model->field_location_id,
+                Job::field_description() => $model->asset_keterangan,
+            ]);
+        });
+
         parent::saving(function ($model)
         {
             if(empty($model->{self::field_code()}))
