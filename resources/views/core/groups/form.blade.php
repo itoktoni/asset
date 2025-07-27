ca517b8c1a2fccba6ab3956a6b0eab927a7fa36f
itok toni laksono <itok.toni@gmail.com>
2025-07-27 11:58:03 +0700

    updat

--- resources/views/core/groups/form.blade.php
+++ resources/views/core/groups/form.blade.php
@@ -7,7 +7,10 @@
                     <x-form-input col="3" name="system_group_code" />
                     <x-form-input col="3" name="system_group_sort" />
                     <x-form-input col="6" name="system_group_name" />
-                    <x-form-input col="6" label="Icon - https://icons.getbootstrap.com" name="system_group_icon" />
+                    <div class="col-md-3 mt-2">
+                        <a class="btn btn-primary mt-4" target="_blank" href="https://icons.getbootstrap.com">https://icons.getbootstrap.com</a>
+                    </div>
+                    <x-form-input col="3" label="Icon" name="system_group_icon" />
                     <x-form-input col="6" name="system_group_url" />
                     <x-form-select col="12" class="tag" multiple name="menu[]" :default="$selected ?? []"
                         :options="$menu" />
